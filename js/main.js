1648e1385857cde70391f46854c386b587c7f9ee
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2024-06-06 23:38:05 +0500

    clients desktop in progress

--- js/main.js
+++ js/main.js
@@ -9,3 +9,27 @@ let swiper = new Swiper(".commentSwiper", {
     },
 });
 // comment slider
+
+// partner slider
+let swiper2 = new Swiper(".partnerSwiper", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    loop: true,
+    navigation: {
+      nextEl: ".comment-button-next",
+      prevEl: ".comment-button-prev",
+    },
+});
+// partner slider
+
+// partner slider
+let swiper3 = new Swiper(".clientsSwiper", {
+    slidesPerView: 7,
+    spaceBetween: 30,
+    loop: true,
+    navigation: {
+      nextEl: ".clients-button-next",
+      prevEl: ".clients-button-prev",
+    },
+});
+// partner slider
